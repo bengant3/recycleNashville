eac5b2f1e0ec7de46dcdad5468f8e9078675540f
bengant3 <bengant3@gmail.com>
2021-11-18 12:10:50 -0600

    started quiz

--- src/pages/Quiz.js
+++ src/pages/Quiz.js
@@ -1,6 +1,25 @@
 import React from 'react';
 
 class Quiz extends React.Component {
+
+    /** Plan of Attack
+     * 
+     * Shows 10 questions sequentially, with feedback (right/wrong/message) after each
+     * Pool of ~20ish questions
+     * 
+     * Components:
+     *  - Frame that holds questions, 4 options/buttons
+     *  - Button
+     *  - Feedback Modal
+     * 
+     * State:
+     *  - question number
+     *  - number correct
+     * 
+     * Redux?
+     * 
+     */
+
     constructor(props) {
         super(props);
         this.state = {
